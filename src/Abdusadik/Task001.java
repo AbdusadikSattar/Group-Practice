09740550db598318033c9fac22dabe98dcc5dd76
ahmetvurdem <71733102+ahmetvurdem@users.noreply.github.com>
2021-02-03 21:11:25 +0300

    Update Task001.java

--- src/Abdusadik/Task001.java
+++ src/Abdusadik/Task001.java
@@ -3,5 +3,6 @@ package Abdusadik;
 public class Task001 {
     public static void main(String[] args) {
         System.out.println("This is first group project for GitHub");
+        // don't worry! ahmet is here
     }
 }
